0f85d484012a2ddd3855328cfdf266eea62b8328
michsayshi <michmichkitten@gmail.com>
2021-02-26 20:02:43 +0000

    complete lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            product_desc: 'Comfy time for your feet! <3'
+        }
+    }
+})
